5ba2ee4016eb7e3b0bbdc65580fa8058db6421f1
TeamSpen210 <spencerb21@live.com>
2020-06-11 16:56:42 +1000

    Fix smashing recipes being rejected as invalid

--- src/main/java/knightminer/inspirations/library/recipe/anvil/AnvilRecipe.java
+++ src/main/java/knightminer/inspirations/library/recipe/anvil/AnvilRecipe.java
@@ -267,7 +267,7 @@ public class AnvilRecipe implements IRecipe<AnvilInventory> {
 				block = null;
 			} else {
 				block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName));
-				if(block == null || block == Blocks.AIR) {
+				if (block == null) {
 					throw new JsonParseException("Unknown block \"" + blockName + "\"");
 				}
 			}
@@ -293,6 +293,7 @@ public class AnvilRecipe implements IRecipe<AnvilInventory> {
 			if(resultName.isEmpty()) {
 				result = null;
 			} else {
+				// Should never be missing, since we've already validated it.
 				result = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(resultName));
 			}
 
